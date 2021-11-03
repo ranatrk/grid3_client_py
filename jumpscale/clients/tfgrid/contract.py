bda49f49738bcbaf35c8cf32a3378412122652d5
ranatrk <rana.t.hassan@gmail.com>
2021-11-03 17:46:59 +0200

    Add docstrings to tfgrid client contract

--- jumpscale/clients/tfgrid/contract.py
+++ jumpscale/clients/tfgrid/contract.py
@@ -8,6 +8,21 @@ class Contract(InterfaceClient):
 
     # create node contract creates a contract
     def create_node_contract(self, nodeID, data, hash, numberOfPublicIPs):
+        """Create/deploy a new node contract
+
+        :param nodeID: nodeId to deploy contract
+        :type nodeID: int
+        :param data: data in contract
+        :type data: str
+        :param hash: deployment hash
+        :type hash: str
+        :param numberOfPublicIPs: number of public IPs to include
+        :type numberOfPublicIPs: int
+        Raises:
+            RuntimeError: if submitted extrinsic failed
+
+        Returns: extrinsic result
+        """
         params = {"node_id": nodeID, "data": data, "deployment_hash": hash, "public_ips": numberOfPublicIPs}
 
         submitted_extrinsic = self.submit_signed_extrinsic(
@@ -47,14 +62,19 @@ class Contract(InterfaceClient):
 
     # getContract gets an contract by id or (hash and node id)
     def get(self, id: int = None, data_hash: str = None, node_id: int = None):
-        """
-            id (int): contract id only
-            OR
-            hash (str): data hash contract was created with
-            node_id (int): node id contract was created with
+        """Get a contract
 
-        Returns:
-            contract : dict
+        :param id: contract id to query [only]
+        :type id: int
+        OR
+        :param hash: data hash contract was created with
+        :type hash: str
+        :param node_id: node id contract was created with
+        :type node_id: int
+        Raises:
+            RuntimeError: if submitted extrinsic failed to get contract id for querying
+
+        :return contract: dict
         """
         if not id:
             if not data_hash or not node_id:
@@ -74,11 +94,12 @@ class Contract(InterfaceClient):
 
     def get_node_contract(self, id: int = None):  # TODO test
         """
-            id (int): contract id only
+        Get node contracts with passed id
 
+        :param id: contract id to query [only]
+        :type id: int
 
-        Returns:
-            contract : list of dict
+        :return contract : list of dict
         """
         if not id:
             raise ValueError("No id provided")
@@ -103,6 +124,12 @@ class Contract(InterfaceClient):
                 raise RuntimeError(submitted_extrinsic.error_message)
 
     def cancel(self, id):
+        """Cancel deployed contract by id
+        :param id: contract id
+        :type id: int
+
+        :return: submitted extrinsic result
+        """
         submitted_extrinsic = self.submit_signed_extrinsic(
             call_module=MODULE_NAME, call_function="cancel_contract", call_params={"contract_id": id}
         )
